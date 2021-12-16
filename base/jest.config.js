bcc3331ce2e6fec8cbefd17503b07d070c1e1935
Raigato <gabriel.raymondou@gmail.com>
2021-12-16 17:01:28 +0100

    jest config

---
+++ base/jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.ts",
+    // "<rootDir>/src/__tests__/mocks/mymock.ts",
+  ],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testRegex: "(/__tests__/.*.(spec|test)).(j|t)sx?$",
+}
